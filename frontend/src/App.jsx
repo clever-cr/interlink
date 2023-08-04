1dd362ee3cae9416687d2004effcf4154209a0a7
Cynthia <109437062+CynthiaUmuhire@users.noreply.github.com>
2023-08-04 23:34:15 +0200

    the basic layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,15 @@ import NavBar from "./components/layout/NavBar";
 import StartButton from "./components/ui/StartButton";
 import Footer from "./components/layout/Footer";
 import StaticsCard from "./components/ui/StatisticsCard";
+import Hero from "./components/sections/hero";
 function App() {
   return (
     <>
       <NavBar />
+
+      <Hero/>
    
-      <StaticsCard />
+      
     
     </>
   );
---
+++ frontend/src/components/sections/Hero.jsx
@@ -0,0 +1,17 @@
+function Hero(){
+    return(
+        <div>
+            <div className="bg-[#F5F5F5]">
+                <div>
+                    <div>
+                        <span>Hey!</span>
+                        <span>We are <strong>Interlink</strong></span>
+                        <span>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful.</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero;
