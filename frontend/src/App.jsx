cc4927956146e35d3a15615cab49a3cd44900635
clever-cr <cleverumuhuza@gmail.com>
2023-08-04 14:59:40 +0200

    add reusable components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import NavBar from "./components/layout/NavBar";
+import StartButton from "./components/ui/StartButton";
 import Footer from "./components/layout/Footer";
+import StaticsCard from "./components/ui/StatisticsCard";
 function App() {
   return (
     <>
       <NavBar />
-      <Footer />
+   
+      <StaticsCard />
+    
     </>
   );
 }
