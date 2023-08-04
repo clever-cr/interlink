99cd6ddcd83c0e90de418aa13ee3c719631d2c1c
clever-cr <cleverumuhuza@gmail.com>
2023-08-04 14:10:48 +0200

    add footer and navbar components

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -1,8 +1,19 @@
-function Footer(){
-return (
+function Footer() {
+  return (
     <>
-    <div></div>
+      <div className="bg-[#141624] text-[#BABABF] flex px-16 items-center justify-between border-t  py-8">
+        <div className="flex items-center gap-10">
+          <h3>Interlink</h3>
+          <p className="text-sm">© interlink 2023. All Rights Reserved.</p>
+        </div>
+
+        <div className="flex text-sm gap-8">
+          <p>Terms</p>
+          <p className="pl-2 border-l border-gray-500">Privacy Policy</p>
+          <p className="pl-2 border-l border-gray-500">"Cookie policy</p>
+        </div>
+      </div>
     </>
-)
+  );
 }
 export default Footer;
