91c479bb4d80994b91e0f8017cb06f954fcdb9cb
Ben6jamin <b.ishimwe1@alustudent.com>
2023-08-06 21:09:35 +0200

    rmv

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,29 +2,21 @@ import NavBar from "./components/layout/NavBar";
 import StartButton from "./components/ui/StartButton";
 import Footer from "./components/layout/Footer";
 import StaticsCard from "./components/ui/StatisticsCard";
-<<<<<<< HEAD
+
 import InternshipPage from './pages/InternshipPage';
 
 
-
-=======
->>>>>>> 1e20ba5d9e933055e3569615a50456dc88c87bee
 function App() {
   return (
     <>
       <NavBar />
-   
+
       <StaticsCard />
-<<<<<<< HEAD
 
-      <div className="internship">
-       <InternshipPage />
 
-       </div>
 
-=======
-    
->>>>>>> 1e20ba5d9e933055e3569615a50456dc88c87bee
+
+
     </>
   );
 }
