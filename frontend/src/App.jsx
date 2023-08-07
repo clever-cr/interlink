d3b0a7dd497918e349a851c15af4c7c15c334017
clever-cr <cleverumuhuza@gmail.com>
2023-08-07 16:46:30 +0200

    refactor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,14 @@ import StartButton from "./components/ui/StartButton";
 import Footer from "./components/layout/Footer";
 import StaticsCard from "./components/ui/StatisticsCard";
 import Hero from "./components/sections/hero";
+import Testimonies from "./components/sections/Testimonies";
 function App() {
   return (
     <>
       <NavBar />
+      <Hero />
 
-      <Hero/>
-   
-      
-    
+      <Footer />
     </>
   );
 }
---
+++ frontend/src/components/sections/Testimonies.jsx
@@ -0,0 +1,8 @@
+function Testimonies() {
+  return (
+    <>
+      <h1 className="text-red-200">hey</h1>
+    </>
+  );
+}
+export default Testimonies;
