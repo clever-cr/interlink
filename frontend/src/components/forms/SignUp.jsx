a884ea4eeb686b2714fd9a8b8106a8248414d538
clever-cr <cleverumuhuza@gmail.com>
2023-08-07 15:40:46 +0200

    add sign up page

--- frontend/src/components/forms/SignUp.jsx
+++ frontend/src/components/forms/SignUp.jsx
@@ -1,6 +1,7 @@
+import Input from "../ui/Input";
 import { AiOutlineArrowRight } from "react-icons/ai";
 
-function Login({}) {
+function SignUp() {
   return (
     <>
       <div className="py-20 max-w-7xl mx-auto h-screen ">
@@ -9,21 +10,19 @@ function Login({}) {
             <h3 className="text-[#141624]  text-4xl">INTERLINK</h3>
             <h2 className="text-2xl bold">Welcome to interlink</h2>
 
-            <form className="flex flex-col gap-8 pt-12">
+            <form className="flex flex-col gap-2 pt-8">
+            <Input placeholder={"First Name"} />
+            <Input placeholder={"Last Name"} />
               <Input placeholder={"email"} />
-              <div className="flex gap-2">
                 <Input placeholder={"password"} />
-                <button className="border px-12 h-[69px] rounded-[23px]">
-                  <AiOutlineArrowRight />
-                </button>
-              </div>
+            
             </form>
-            <div className="pt-12 justify-center- flex">
+            <div className="pt-5 justify-center- flex">
               <button className="bg-[#141624] px-14 py-3 rounded-l-[23px] text-white">
-                Log In
+            Signup
               </button>
               <button className=" px-14 py-3 rounded-r-[23px] bg-white">
-                Sign Up
+                Log In
               </button>
             </div>
           </div>
@@ -33,4 +32,4 @@ function Login({}) {
     </>
   );
 }
-export default Login;
+export default SignUp;
---
+++ frontend/src/components/ui/Input.jsx
@@ -0,0 +1,11 @@
+function Input({name,value,placeholder}) {
+  return (
+    <input
+      className="outline-[#141624] w-[446px] h-[69px] px-3 rounded-[23px] border"
+      name={name}
+      value={value}
+      placeholder={placeholder}
+    />
+  );
+}
+export default Input;
