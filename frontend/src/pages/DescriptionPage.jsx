ea3fae4bf554961f1c9202d6c7ccec3d8577ceaf
uwicyeza christa <109435174+christauwicyeza@users.noreply.github.com>
2023-08-08 20:46:04 +0200

    updated description page

--- frontend/src/pages/DescriptionPage.jsx
+++ frontend/src/pages/DescriptionPage.jsx
@@ -1,5 +1,6 @@
 function DescriptionPage(){
-    return(<><div className="padding px-32 py-20" > <h3 className="text-sm bg-[#4B6BFB] text-white rounded-lg shadow-sm"> Technology  </h3> </div>
-        </>)
+    return(<><div className="padding px-32 py-5" > <h3 className="text-sm bg-[#4B6BFB] text-white rounded-lg shadow-sm"> Technology  </h3> </div>
+        <div className="padding px-32 py-5"><h4 className="text-2xl font-bold font-mono"> frontend developer intern </h4> </div ></>)
+
 }
 export default DescriptionPage;
