2bc1ba314cddb05f6ac373c54e5f3942c67d9e93
clever-cr <cleverumuhuza@gmail.com>
2023-08-10 13:02:51 +0200

    refactor

--- frontend/src/pages/DescriptionPage.jsx
+++ frontend/src/pages/DescriptionPage.jsx
@@ -1,6 +1,7 @@
 import StartButton from "../components/ui/StartButton";
 import { useParams } from "react-router";
 import { opportunities } from "../data/Opportunites";
+import { Link } from "react-router-dom";
 
 function DescriptionPage() {
   const { id } = useParams();
@@ -10,52 +11,54 @@ function DescriptionPage() {
 
   return (
     <>
-      <div className="padding px-32 py-5">
-        {" "}
-        <h3 className="text-sm bg-[#4B6BFB] text-white rounded-lg shadow-sm">
-          {" "}
-          {opportunity.title}
-        </h3>{" "}
-      </div>
-      <div className="padding px-32 py-5">
-        <h4 className="text-2xl font-bold font-mono">
-          {" "}
-          frontend developer intern{" "}
-        </h4>{" "}
-      </div>
-      <div class="flex items-center space-x-2">
-        <div class="h-4 w-4 padding px-32 ">
-          <img
-            src="frontend\src\pages\avatar.jpg"
-            alt="User Icon"
-            class="h-full w-full "
-          ></img>
+      <div className="px-32 h-screen- py-20 flex flex-col gap-5">
+        <div className="w-40">
+          <h3 className="text-sm bg-[#4B6BFB] text-white rounded-lg shadow-sm py-3 px-2">
+            {opportunity.title}
+          </h3>
+        </div>
+        <div className="padding py-3">
+          <h1 className="text-5xl font-bold font-mono- max-w-6xl leading-[60px]">
+            {opportunity.text}
+          </h1>{" "}
+        </div>
+        <div className="flex items-center gap-10 text-[#97989F]">
+          <p>{opportunity.category}</p>
+          <p>{opportunity.deadline}</p>
+        </div>
+        <div class="flex items-center space-x-2">
+          <div class=" padding px-32- w-full pt-8">
+            <img
+              className="w-full rounded-[23px]"
+              src={opportunity.photoPath}
+              alt={opportunity.title}
+            />
+          </div>
         </div>
 
-        <span class="text-xs text-gray-500">John Doe</span>
-
-        <span class="text-xs text-gray-500">August 8, 2023</span>
-      </div>
-      <div className="padding px-32 ">
-        <h5 className="text-xl font-bold font-mono">
-          {" "}
-          frontend developer intern{" "}
-        </h5>{" "}
-      </div>
-      <div className="padding px-32 py-5">
-        <p className="text-lg  font-serif">
-          {" "}
-          Join our dynamic team as a [Job Title] Intern at [Company Name]! This
-          [Duration] internship offers hands-on experience in [specific tasks or
-          projects], providing a unique opportunity to learn and contribute to
-          our [industry/field] initiatives. Ideal for students or recent
-          graduates with strong [relevant skills], this internship promises
-          insights into [industry/field] practices and valuable networking
-          opportunities.{" "}
-        </p>{" "}
-      </div>
-      <div className="padding px-32 py-5">
-        <StartButton text="apply" />
+        <div className="padding py-5">
+          <p className="text-lg  font-serif">{opportunity.description}</p>
+          <p>
+            Before embarking on your journey, take the time to research your
+            destination. This includes understanding the local culture, customs,
+            and laws, as well as identifying top attractions, restaurants, and
+            accommodations. Doing so will help you navigate your destination
+            with confidence and avoid any cultural faux pas. Lorem ipsum dolor
+            sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. In hendrerit gravida
+            rutrum quisque non tellus orci ac auctor. Mi ipsum faucibus vitae
+            aliquet nec ullamcorper sit amet. Aenean euismod elementum nisi quis
+            eleifend quam adipiscing vitae. Viverra adipiscing at in tellus.
+          </p>
+        </div>
+        <div className="">
+          <a
+            href="https://www.greatrwandajobs.com/jobs/job-detail/job-Management-Trainee-Programme-job-at-IM-Bank-Rwanda-PLC,-30478?utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic"
+            target="_blank"
+          >
+            <StartButton text="Apply" />
+          </a>
+        </div>
       </div>
     </>
   );
