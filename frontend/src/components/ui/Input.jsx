95525e498fba48a5c909fd65f65bad799e5a355d
clever-cr <cleverumuhuza@gmail.com>
2023-08-16 09:13:15 +0200

    refactor

--- frontend/src/components/ui/Input.jsx
+++ frontend/src/components/ui/Input.jsx
@@ -1,5 +1,5 @@
 function Input({ name, value, placeholder, ...props }) {
-  return (
+  return ( 
     <input
       className="outline-[#141624] w-[446px] h-[69px] px-3 rounded-[23px] border"
       name={name}
